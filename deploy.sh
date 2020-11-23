ab184f08390d37997d11fbd78eb2b2aea516a2c7
Charly Marchiaro <carlosmarchiaro@gmail.com>
2020-11-22 21:32:56 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 SHA=$(git rev-parse HEAD)
 
-docker build -t charlymarchiaro/scrapoxy:latest -t charlymarchiaro/scrapoxy:$SHA -f ./Dockerfile .
-docker push charlymarchiaro/scrapoxy:latest
-docker push charlymarchiaro/scrapoxy:$SHA
+docker build -t charlymarchiaro/scrapoxy-auth:latest -t charlymarchiaro/scrapoxy-auth:$SHA -f ./Dockerfile .
+docker push charlymarchiaro/scrapoxy-auth:latest
+docker push charlymarchiaro/scrapoxy-auth:$SHA
